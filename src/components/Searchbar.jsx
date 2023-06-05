916dbccc485f1cbffccb0ae0b0fabb220924fbcb
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-06-05 21:41:14 +0200

    components created

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import css from 'styles.module.css'
+
+
+export default class Searchbar extends Component {
+  render() {
+    return (
+        <header className={css.Searchbar}>
+        <form className={css.SearchForm}>
+          <button type="submit" className={css.SearchFormButton}>
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+      
+          <input
+            className={css.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    )
+  }
+}
+
