db5f34a7b27a21c60a0ff2abd2925cc30c6f729d
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-06-12 14:56:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -46,7 +46,7 @@ export default class App extends Component {
       page: this.state.page,
       per_page: 12,      
     });
-    
+
       const response = await axios.get('https://pixabay.com/api/?' + searchParams)
       const galleryItems = response.data.hits;
       
